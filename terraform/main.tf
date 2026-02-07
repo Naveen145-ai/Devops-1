2db22b5f86842199f92547b9ee31e1599e36d163
naveensrinivas <naveensrinivas145@gmail.com>
2026-02-07 11:50:57 +0530

    msg

---
+++ terraform/main.tf
@@ -0,0 +1,49 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+# -------------------
+# VPC
+# -------------------
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.5.1"
+
+  name = "student-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-1a", "us-east-1b"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Project   = "student-devops"
+  }
+}
+
+# -------------------
+# EKS
+# -------------------
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "20.8.4"
+
+  cluster_name    = "student-eks"
+  cluster_version = "1.29"
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  eks_managed_node_groups = {
+    nodes = {
+      desired_size   = 2
+      min_size       = 1
+      max_size       = 2
+      instance_types = ["t3.small"]   # minimum safe
+    }
+  }
+}
