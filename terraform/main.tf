1aa27c838ea1aaa6b0cfa60e7c89451dbf8e4015
naveensrinivas <naveensrinivas145@gmail.com>
2026-02-07 14:21:53 +0530

    msg

--- terraform/main.tf
+++ terraform/main.tf
@@ -52,7 +52,7 @@ module "eks" {
       desired_size   = 2
       min_size       = 1
       max_size       = 2
-      instance_types = ["t3.micro"]
+      instance_types = ["t3.medium"]
     }
   }
 }
